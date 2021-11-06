127c4551b69f609c5efbb249b6f927b6c5b8a3f6
valik94 <valentin.lyashenko@ryerson.ca>
2021-11-06 18:51:30 +0000

    modified slightly the wordSearch to optimize and include intial test cases

--- wordsearch.js
+++ wordsearch.js
@@ -32,18 +32,18 @@ const wordSearch = (letters, word) => {
       else {return false}
   }
 }
-// const result = wordSearch([
-//     ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
-//     ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
-//     ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
-//     ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
-//     ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
-//     ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
-//     ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
-//     ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
-//     ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-//   ], 'FRANK');
+const result = wordSearch([
+    ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+    ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+    ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+    ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+    ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+    ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+    ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+    ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+    ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+  ], 'FRANK');
 
-// wordSearch([], 'FRANK');
+wordSearch([], 'FRANK');
 
 module.exports = wordSearch
